ce60c0ed67343c8e6ac0614cd5f388e18f737e3c
linbaishuang <1765259718@qq.com>
2017-12-22 20:03:27 +0800

    完成

--- main/main.js
+++ main/main.js
@@ -1,6 +1,31 @@
 // Write your cade below:
-function main() {};
+module.exports = function main(wordA, wordB) {
+    var reA = checkA(wordA, wordB);
+    var reB = checkB(wordA, wordB);
+    return reA + reB;
+};
 
-module.exports = {
-    main
+function checkA(woA,woB) {
+    var count1 = 0;
+    for(var i=0;i<woA.length;i++){
+        for(var j=0;j<woB.length;j++){
+            if(i === j && woA[i] === woB[j]){
+                count1++;
+            }
+        }
+    }
+    return count1 + "A";
 }
+
+function checkB(woA,woB) {
+    var count2 = 0;
+    for(var i=0;i<woA.length;i++){
+        for(var j=0;j<woB.length;j++){
+            if(i !== j && woA[i] === woB[j]){
+                count2++;
+            }
+        }
+    }
+    return count2 + "B";
+}
+
--- spec/main-spec.js
+++ spec/main-spec.js
@@ -1,5 +1,104 @@
 const main = require('../main/main');
 
 describe('main()', () => {
+    it("the first case", () => {
+        let random = "1234";
+        let input = "4312";
+        let result = main(random, input);
+        expect(result).toEqual("0A4B");
+    });
+
+    it("the second case", () => {
+        let random = "1234";
+        let input = "0321";
+        let result = main(random, input);
+        expect(result).toEqual("0A3B");
+    });
+
+    it("the third case", () => {
+        let random = "1234";
+        let input = "0043";
+        let result = main(random, input);
+        expect(result).toEqual("0A2B");
+    });
+
+    it("the forth case", () => {
+        let random = "1234";
+        let input = "0003";
+        let result = main(random, input);
+        expect(result).toEqual("0A1B");
+    });
+
+    it("the fifth case", () => {
+        let random = "1234";
+        let input = "0000";
+        let result = main(random, input);
+        expect(result).toEqual("0A0B");
+    });
+
+    it("the sixth case", () => {
+        let random = "1234";
+        let input = "1423";
+        let result = main(random, input);
+        expect(result).toEqual("1A3B");
+    });
+
+    it("the seventh case", () => {
+        let random = "1234";
+        let input = "1340";
+        let result = main(random, input);
+        expect(result).toEqual("1A2B");
+    });
+
+    it("the eighth case", () => {
+        let random = "1234";
+        let input = "1002";
+        let result = main(random, input);
+        expect(result).toEqual("1A1B");
+    });
+
+    it("the ninth case", () => {
+        let random = "1234";
+        let input = "1000";
+        let result = main(random, input);
+        expect(result).toEqual("1A0B");
+    });
+
+    it("the tenth case", () => {
+        let random = "1234";
+        let input = "1243";
+        let result = main(random, input);
+        expect(result).toEqual("2A2B");
+    });
+
+    it("the eleventh case", () => {
+        let random = "1234";
+        let input = "1203";
+        let result = main(random, input);
+        expect(result).toEqual("2A1B");
+    });
+
+    it("the twlfth case", () => {
+        let random = "1234";
+        let input = "1200";
+        let result = main(random, input);
+        expect(result).toEqual("2A0B");
+    });
+
+    it("the thirteenth case", () => {
+        let random = "1234";
+        let input = "1230";
+        let result = main(random, input);
+        expect(result).toEqual("3A0B");
+    });
+
+    it("the fourteenth case", () => {
+        let random = "1234";
+        let input = "1234";
+        let result = main(random, input);
+        expect(result).toEqual("4A0B");
+    })
+
+    //3A1B的情况不存在
 
 });
